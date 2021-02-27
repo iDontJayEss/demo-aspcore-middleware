661f091b7112865edeb987bbe61d65bfd4095aaa
Ian Schlarman <ischlarman@gmail.com>
2021-02-26 21:14:45 -0600

    Added controller contracts.

---
+++ src/Demo.Services.Contracts/Library/IAccountController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Services.Contracts.Library
+{
+    interface IAccountController
+    {
+    }
+}
--- src/Demo.Services.Models/Library/Author.cs
+++ src/Demo.Services.Models/Library/Author.cs
@@ -2,12 +2,24 @@
 
 namespace Demo.Services.Models.Library
 {
+    /// <summary>
+    /// Contains information about the author of <see cref="Title"/>.
+    /// </summary>
     public class Author : ResourceBase
     {
+        /// <summary>
+        /// The full name of the author.
+        /// </summary>
         public string Name { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Biographical information about the author.
+        /// </summary>
         public string Biography { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The date of birth of the author.
+        /// </summary>
         public DateTime? BirthDate { get; set; }
     }
 }
